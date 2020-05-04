5cb152c2a1d5775bd9aaaf74d61824679d79e427
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-05-03 21:58:14 -0400

    Authentication middleware added

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,23 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+  const token = req.headers.authorization
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ errorMessage: "There was an error with the token!" });
+      } else {
+        req.decodedJwt = decodedToken;
+        console.log(req.decodedJwt);
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ errorMessage: "You won't get past this!" });
+  }
+}
