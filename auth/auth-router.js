49cd4209d8aa28d40c1a682656bc2311095a32bf
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-05-03 21:29:12 -0400

    Login and GenToken built out

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -14,14 +14,43 @@ router.post('/register', (req, res) => {
       res.status(201).json({ message: `Welcome to Dad Jokes, ${user.username}!` })
     })
     .catch((err) => {
-      res.status(500).json({ errorMessage: "There was an error registering you!", err })
+      res.status(500).json({ errorMessage: "There was an error with registering!", err })
     })
 
 
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  db.findBy({ username })
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user)
+
+        res.status(200).json({ message: `You've been logged in, ${user.username}!`, token })
+      } else {
+        res.status(401).json({ errorMessage: "Your credentials were invalid." })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "There was an error with logging you in!", err })
+    })
 });
+
+const genToken = user => {
+  const payload = {
+    id: user.id,
+    username: user.username,
+  }
+
+  const options = {
+    expiresIn: "8h",
+  }
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+
+  return token;
+}
 
 module.exports = router;
