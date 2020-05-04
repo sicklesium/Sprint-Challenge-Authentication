37265133835ebcc071428ae1746a53416007a372
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-05-03 21:18:56 -0400

    Register endpoint built out

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const db = require('../api/dbModel.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  user.password = hash;
+
+  db.add(user)
+    .then((user) => {
+      res.status(201).json({ message: `Welcome to Dad Jokes, ${user.username}!` })
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "There was an error registering you!", err })
+    })
+
+
 });
 
 router.post('/login', (req, res) => {
